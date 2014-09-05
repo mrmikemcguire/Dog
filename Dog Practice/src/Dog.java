c492ccf387938481510386f5c0f1dd1ef8951301
Mike McGuire <mcguire@mullenhigh.com>
2014-09-05 16:39:04 -0600

    Tidied up

--- Dog Practice/src/Dog.java
+++ Dog Practice/src/Dog.java
@@ -1,31 +1,26 @@
-
-//import java.util.ArrayList;
-import java.util.ArrayList;
-import static java.lang.System.*;
-
 public class Dog
-{
+	{
 	private int age;
 	private String name;
 	
 	public Dog( String n, int a )
-	{
+		{
 		age = a;
 		name = n;
-	}
+		}
 	
 	public int getAge()
-	{
+		{
 		return age;
-	}
+		}
 	
 	public String getName()
-	{
+		{
 		return name;
-	}
+		}
 	
 	public String toString()
-	{
-		return "Dog - " + name + " " + age;
+		{
+		return name + " " + age;
+		}
 	}
-}
--- Dog Practice/src/DoggiesRunner.java
+++ Dog Practice/src/DoggiesRunner.java
@@ -1,17 +1,9 @@
-
-import java.util.List;
 import java.util.ArrayList;
-import static java.lang.System.*;
 
 public class DoggiesRunner
-
-{
-	//make a method to find the oldest Dog
-	//this method should return a reference
-	//to the Dog that is the oldest
-	
-	public static Dog findOldest( ArrayList<Dog> kennel )
 	{
+	public static Dog findOldest( ArrayList<Dog> kennel )
+		{
 		Dog oldestDog = null;
 		int max = 0;
 		for(Dog d : kennel)
@@ -22,12 +14,11 @@ public class DoggiesRunner
 				oldestDog = d;
 				}
 			}
-
 		return oldestDog;		
-	}
+		}
 	
 	public static void main(String args[])
-	{
+		{
 		ArrayList<Dog> kennel = new ArrayList<Dog>();
 		
 		kennel.add( new Dog( "Fred", 7) );
@@ -35,11 +26,7 @@ public class DoggiesRunner
 		kennel.add( new Dog( "Sundae", 1) );
 		kennel.add( new Dog( "Ficus", 10) );
 
-		System.out.println( kennel );
-
-		//call the method to find the oldest Dog
-		//print the oldest Dog
-		
-		System.out.println( "The oldest dog: " + findOldest( kennel ) );
+		System.out.println(kennel);
+		System.out.println("The oldest dog: " + findOldest( kennel));
+		}
 	}
-}
